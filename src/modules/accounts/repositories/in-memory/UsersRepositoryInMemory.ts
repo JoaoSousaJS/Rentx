5dbb51115ba2d126e6634e628e55a6d9ed75b33e
Joao Sousa <joao.rsousa23@gmail.com>
2022-06-29 23:47:47 +0930

    feat: add UsersRepositoryInMemory

---
+++ src/modules/accounts/repositories/in-memory/UsersRepositoryInMemory.ts
@@ -0,0 +1,52 @@
+import { Users } from "@prisma/client";
+
+import { ICreateUserDTO } from "../../dtos/ICreateUserDTO";
+import { IUserRepository } from "../IUserRepository";
+
+class UsersRepositoryInMemory implements IUserRepository {
+  private users: Users[] = [];
+
+  public async findByEmail(email: string): Promise<Users | undefined> {
+    const user = this.users.find((user) => user.email === email);
+
+    return user;
+  }
+
+  public async findById(id: string): Promise<Users | undefined> {
+    const user = this.users.find((user) => user.id === id);
+
+    return user;
+  }
+
+  async updateAvatar(userId: string, avatarFile: string): Promise<void> {
+    const user = await this.findById(userId);
+
+    if (!user) {
+      throw new Error("User not found");
+    }
+
+    user.avatar = avatarFile;
+  }
+
+  public async create({
+    name,
+    email,
+    password,
+    driverLicense,
+    avatar,
+  }: ICreateUserDTO): Promise<void> {
+    this.users.push({
+      name,
+      email,
+      password,
+      driverLicense,
+      avatar,
+      id: "",
+      isAdmin: false,
+      createdAt: undefined,
+      updatedAt: undefined,
+    });
+  }
+}
+
+export { UsersRepositoryInMemory };

c5be1e8a591d2c854524e91186a2595e744b28c1
Joao Sousa <joao.rsousa23@gmail.com>
2022-07-04 00:38:20 +0930

    feat: add ICreateCarDTO

---
+++ src/modules/cars/dtos/ICreateCarDTO.ts
@@ -0,0 +1,9 @@
+export interface ICreateCarDto {
+  name: string;
+  description: string;
+  dailyRate: number;
+  licensePlate: string;
+  fineAmount: number;
+  brand: string;
+  categoryId: string;
+}

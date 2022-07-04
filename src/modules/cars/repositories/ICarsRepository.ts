73f64c6927b294af145609d44448d894439e2520
Joao Sousa <joao.rsousa23@gmail.com>
2022-07-05 00:10:00 +0930

    feat: add findByLicensePlate

--- src/modules/cars/repositories/ICarsRepository.ts
+++ src/modules/cars/repositories/ICarsRepository.ts
@@ -5,6 +5,7 @@ import { ICreateCarDto } from "../dtos/ICreateCarDTO";
 interface ICarsRepository {
   create(data: ICreateCarDto): Promise<void>;
   list(): Promise<Cars[]>;
+  findByLicensePlate(licensePlate: string): Promise<Cars | undefined>;
 }
 
 export { ICarsRepository };

ab55b0ba08dcebf70b69a71a96c6091b4ddbe226
Joao Sousa <joao.rsousa23@gmail.com>
2022-07-05 18:47:32 +0930

    feat: add CarsRepository

---
+++ src/modules/cars/infra/prisma/repositories/CarsRepository.ts
@@ -0,0 +1,52 @@
+import { ICreateCarDto } from "@modules/cars/dtos/ICreateCarDTO";
+import { ICarsRepository } from "@modules/cars/repositories/ICarsRepository";
+import { Cars, PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+class CarsRepository implements ICarsRepository {
+  private readonly repository: PrismaClient["cars"];
+  constructor() {
+    this.repository = prisma.cars;
+  }
+
+  async create({
+    name,
+    description,
+    brand,
+    dailyRate,
+    fineAmount,
+    licensePlate,
+    categoryId,
+  }: ICreateCarDto): Promise<Cars> {
+    const car = await this.repository.create({
+      data: {
+        name,
+        description,
+        brand,
+        dailyRate,
+        fineAmount,
+        licensePlate,
+        categoryId,
+        available: true,
+      },
+    });
+
+    return car;
+  }
+
+  async list(): Promise<Cars[]> {
+    const cars = await this.repository.findMany();
+    return cars;
+  }
+
+  async findByLicensePlate(licensePlate: string): Promise<Cars> {
+    const car = await this.repository.findUnique({
+      where: { licensePlate },
+    });
+
+    return car;
+  }
+}
+
+export { CarsRepository };

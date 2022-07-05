bad2e3ee0d6e835e63fd28d15f5763ef90715133
Joao Sousa <joao.rsousa23@gmail.com>
2022-07-05 18:50:42 +0930

    feat: add CarsRepository to container

--- src/shared/container/index.ts
+++ src/shared/container/index.ts
@@ -2,8 +2,10 @@ import { container } from "tsyringe";
 
 import { UsersRepository } from "@modules/accounts/infra/prisma/repositories/UserRepository";
 import { IUserRepository } from "@modules/accounts/repositories/IUserRepository";
+import { CarsRepository } from "@modules/cars/infra/prisma/repositories/CarsRepository";
 import { CategoriesRepository } from "@modules/cars/infra/prisma/repositories/CategoriesRepository";
 import { SpecificationRepository } from "@modules/cars/infra/prisma/repositories/SpecificationRepository";
+import { ICarsRepository } from "@modules/cars/repositories/ICarsRepository";
 import { ICategoriesRepository } from "@modules/cars/repositories/ICategoriesRepository";
 import { ISpecificationRepository } from "@modules/cars/repositories/ISpecificationRepository";
 
@@ -21,3 +23,5 @@ container.registerSingleton<IUserRepository>(
   "UsersRepository",
   UsersRepository
 );
+
+container.registerSingleton<ICarsRepository>("CarsRepository", CarsRepository);
